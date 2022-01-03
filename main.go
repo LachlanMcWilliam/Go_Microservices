a78454a2ffa2b829a28a4ca47bc6dfdccc06a413
Lachlan McWilliam <mcwilliam01@gmail.com>
2022-01-03 19:14:14 +0000

    Added graceful shutdown functionallity

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 
 	"github.com/lachlanmcwilliam/microservices/handlers"
@@ -30,5 +32,24 @@ func main() {
 		WriteTimeout: 1 * time.Second,
 	}
 
-	s.ListenAndServe()
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	// Create a channel to listen for an interrupt or terminate signal from the OS.
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	// Block until we receive our signal.
+	sig := <-sigChan
+	l.Println("Received shutdown signal, gracefully shutting down:", sig)
+
+	// Create context with timeout
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	// Shutdown the server and clean up resources
+	s.Shutdown(ctx)
 }
